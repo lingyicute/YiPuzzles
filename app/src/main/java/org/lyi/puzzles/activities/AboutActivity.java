87bfb73549edde0561a4db93acfb3827b8895181
lingyicute <160479206+lingyicute@users.noreply.github.com>
2026-09-19 13:53:24 +0000

    fix build error

--- app/src/main/java/org/lyi/puzzles/activities/AboutActivity.java
+++ app/src/main/java/org/lyi/puzzles/activities/AboutActivity.java
@@ -64,8 +64,6 @@ public class AboutActivity extends AppCompatActivity {
 
         overridePendingTransition(0, 0);
 
-        ((TextView) findViewById(R.id.secusoWebsite)).setMovementMethod(LinkMovementMethod.getInstance());
-        ((TextView) findViewById(R.id.githubURL)).setMovementMethod(LinkMovementMethod.getInstance());
         ((TextView) findViewById(R.id.textFieldVersionName)).setText(getString(R.string.version_number, BuildConfig.VERSION_NAME));
     }
 
