650f6755f8f897eb3bfaba7b8631361c07eeeb70
lingyicute <160479206+lingyicute@users.noreply.github.com>
2026-09-20 04:48:09 +0000

    minor strings change

--- app/src/main/java/org/lyi/puzzles/PF2048.kt
+++ app/src/main/java/org/lyi/puzzles/PF2048.kt
@@ -1,18 +1,18 @@
 /*
  This file is part of YiPuzzles.
 
- Privacy Friendly Sudoku is free software:
+ YiPuzzles is free software:
  you can redistribute it and/or modify it under the terms of the
  GNU General Public License as published by the Free Software Foundation,
  either version 3 of the License, or any later version.
 
- Privacy Friendly Sudoku is distributed in the hope
+ YiPuzzles is distributed in the hope
  that it will be useful, but WITHOUT ANY WARRANTY; without even
  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  See the GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Privacy Friendly Sudoku. If not, see <http://www.gnu.org/licenses/>.
+ along with YiPuzzles. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.lyi.puzzles
 
